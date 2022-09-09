f44c66f0991348a4c8c31ab91a82dffdf2bddb33
don <doncezario.101@gmail.com>
2022-09-09 20:34:34 +0300

    Connect and check existing tables

--- data/sql.go
+++ data/sql.go
@@ -1,5 +1,36 @@
 package data
 
-func Hh() {
-	return
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"log"
+)
+
+var db *sql.DB
+
+func Start(user, password string) error {
+	var err error
+	db, err = sql.Open("pgx", fmt.Sprintf("postgres://%s:%s@localhost:5432", user, password))
+	if err != nil {
+		return err
+	}
+	rows, err := db.Query("SELECT tablename FROM pg_catalog.pg_tables " +
+		"WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var table string
+		err = rows.Scan(&table)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"RestApi/data"
+	"log"
+)
 
+func main() {
+	if err := data.Start("postgres", "postgres"); err != nil {
+		log.Fatal(err.Error())
+	}
 }
